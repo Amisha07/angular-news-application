628e0ee567b71d211e5c031fe4c5ba23928c24e9
Amisha07 <amishagadhia1707@gmail.com>
2020-06-03 16:21:24 -0500

    100 binaries uploaded

--- src/app/news-api.service.ts
+++ src/app/news-api.service.ts
@@ -9,15 +9,23 @@ export class NewsApiService {
 
   api_key = 'e005ed30cf2b4807adb1cf78d2fbecfa';
 
+
   constructor(private http:HttpClient) { }
+
+
   initSources(){
-     return this.http.get('https://newsapi.org/v2/sources?country=us&language=en&apiKey='+this.api_key);
+	 return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
   }
+
+
   initArticles(){
-   return this.http.get('https://newsapi.org/v2/top-headlines?sources=cnn&apiKey='+this.api_key);
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
   }
-  
+
   getArticlesByID(source: String){
    return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
   }
-} 
+
+
+
+}
